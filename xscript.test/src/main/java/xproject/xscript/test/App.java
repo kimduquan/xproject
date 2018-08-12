480739d1fae16e3e97f8fe8f878a56000a722d04
John Doe <john@sample.com>
2018-08-12 21:58:09 +0700

    add some code

--- xscript.test/src/main/java/xproject/xscript/test/App.java
+++ xscript.test/src/main/java/xproject/xscript/test/App.java
@@ -1,44 +1,51 @@
 package xproject.xscript.test;
 
-import org.openqa.selenium.Keys;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Core;
+import org.opencv.core.DMatch;
+import org.opencv.core.KeyPoint;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.Feature2D;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
 
-import xproject.xlang.XClassLoader;
-import xproject.xlang.XFactory;
-import xproject.xlang.impl.XClassLoaderImpl;
-import xproject.xlang.impl.XFactoryImpl;
-import xproject.xscript.XBindings;
-import xproject.xscript.impl.XBindingsImpl;
-import xproject.xscript.impl.model.XCommandFactory;
-import xproject.xscript.impl.model.XScriptEngineModel;
-import xproject.xutil.XScanner;
-import xproject.xutil.impl.XScannerImpl;
+import com.google.common.io.Files;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*System.setProperty("webdriver.edge.driver", "D:\\installed\\MSWebdriver\\MicrosoftWebDriver.exe");
-        EdgeDriver driver = new EdgeDriver();
-        driver.get("http://www.google.com");
-        WebElement input = driver.findElementById("lst-ib");
-        assert(input != null);
-        input.sendKeys("abc");
-        WebElement button = driver.findElementByName("btnK");
-        assert(button != null);
-        button.click();
-        try
-        {
-            WebElement abc = driver.findElementByPartialLinkText("ABC.com");
-        }
-        catch(Exception ex)
-        {
-        	
-        }
-        driver.close();*/
+		System.setProperty("webdriver.edge.driver", "D:\\installed\\MSWebdriver\\MicrosoftWebDriver.exe");
+        System.setProperty("TESSDATA_PREFIX", "D:\\installed\\Tesseract-OCR");
+		EdgeDriver driver = new EdgeDriver();
+		//test(driver, "https://www.google.com/", By.name("btnK"), "sub_image.png", "image.png");
+		//test(driver, "https://www.w3schools.com/", By.partialLinkText("Try it Yourself »"), "sub_image.png", "image.png");
+		//test(driver, "https://www.google.com/", By.id("lst-ib"), "sub_image.png", "image.png");
+		test(driver, "https://www.google.com/", By.id("sfdiv"), "sub_image.png", "image.png");
+		driver.close();
+		
         
-        XFactory xfactory = XFactoryImpl.xnew();
+        /*XFactory xfactory = XFactoryImpl.xnew();
         try {
 			xfactory.xref(xfactory);
 		} catch (Exception e1) {
@@ -49,15 +56,227 @@ public class App {
         XClassLoader xclassLoader = XClassLoaderImpl.xnew(ClassLoader.getSystemClassLoader(), xfactory);
         XScriptEngineModel engine = new XScriptEngineModel(xfactory, xcommandFactory, xclassLoader);
         XScanner xscanner = XScannerImpl.xnew("File", "test.xs");
-        XBindings xbindingsEngine = XBindingsImpl.xnew();
-        XBindings xbindingsGlobal = XBindingsImpl.xnew();
         XBindings xbindings = XBindingsImpl.xnew();
         try {
 			engine.xeval(xscanner, xbindings);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 
+	
+	protected static MatOfKeyPoint detect(String detectorName, Feature2D detector, Mat image)
+	{
+		MatOfKeyPoint points = new MatOfKeyPoint();
+		long start = System.currentTimeMillis();
+		detector.detect(image, points);
+		System.out.println(detectorName + "detect :" + (System.currentTimeMillis() - start));
+		return points;
+	}
+	
+	protected static Mat compute(String computerName, Feature2D computer, Mat image, MatOfKeyPoint points)
+	{
+		Mat descriptors = new Mat();
+		long start = System.currentTimeMillis();
+		computer.compute(image, points, descriptors);
+		System.out.println(computerName + " compute :" + (System.currentTimeMillis() - start));
+		return descriptors;
+	}
+	
+	protected static MatOfDMatch match(String matcherName, DescriptorMatcher matcher, Mat subImageDescriptors, Mat imageDescriptors)
+	{
+		MatOfDMatch matches = new MatOfDMatch();
+		long start = System.currentTimeMillis();
+		matcher.match(subImageDescriptors, imageDescriptors, matches);
+		System.out.println(matcherName + " match :" + (System.currentTimeMillis() - start));
+		return matches;
+	}
+	
+	protected static Rect check(String subImageName, Mat subImage, String imageName, Mat image, String matcherName, DescriptorMatcher matcher, String computerName, Feature2D computer, String detectorName, Feature2D detector)
+	{
+		MatOfKeyPoint subImagePoins = detect(detectorName, detector, subImage);
+		Mat subImageDetected = new Mat();
+		Features2d.drawKeypoints(subImage, subImagePoins, subImageDetected);
+		//Imgcodecs.imwrite(matcherName + "_" + computerName + "_" + detectorName + "_" + subImageName, subImageDetected);
+		Mat subImageDesc = compute(computerName, computer, subImage, subImagePoins);
+		
+		MatOfKeyPoint imagePoints = detect(detectorName, detector, image);
+		Mat imageDetected = new Mat();
+		Features2d.drawKeypoints(image, imagePoints, imageDetected);
+		//Imgcodecs.imwrite(matcherName + "_" + computerName + "_" + detectorName + "_" + imageName, imageDetected);
+		Mat imageDesc = compute(computerName, computer, image, imagePoints);
+		
+		MatOfDMatch matches = match(matcherName, matcher, subImageDesc, imageDesc);
+		
+		Mat matchedImage = new Mat();
+		Features2d.drawMatches(subImage, subImagePoins, image, imagePoints, matches, matchedImage);
+		Imgcodecs.imwrite(matcherName + "_" + computerName + "_" + detectorName + "_" + "matched.png", matchedImage);
+		
+		Rect rect = convert2(matches, imagePoints);
+		System.out.println("converted : " + rect);
+		return rect;
+	}
+	
+	protected static void test(RemoteWebDriver driver, String subImageName, Mat subImage, String imageName, Mat image, Map<String, DescriptorMatcher> matchers, Map<String, Feature2D> computers, Map<String, Feature2D> detectors)
+	{
+		for(Entry<String, DescriptorMatcher> matcher : matchers.entrySet())
+		{
+			for(Entry<String, Feature2D> computer : computers.entrySet())
+			{
+				for(Entry<String, Feature2D> detector : detectors.entrySet())
+				{
+					try
+					{
+						Rect rect = check(subImageName, subImage, imageName, image,matcher.getKey(), matcher.getValue(), computer.getKey(), computer.getValue(), detector.getKey(), detector.getValue());
+						//RemoteWebElement element = (RemoteWebElement) driver.executeScript("return document.elementFromPoint(" + (rect.x + rect.width / 2) + "," +  (rect.y + rect.height / 2) + ");");
+						//element.click();
+						/*Tesseract tess = new Tesseract();
+						Mat ocr = image.submat(rect);
+						Imgcodecs.imwrite("ocr.png", ocr);
+						String text = tess.doOCR(new File("ocr.png"));
+						System.out.println("recorded text : " + text);*/
+						
+					}
+					catch(Exception ex)
+					{
+						System.out.println("check failed " + String.join("|", matcher.getKey(), computer.getKey(), detector.getKey()) + " caused : " + ex.getMessage());
+					}
+				}
+			}
+		}
+	}
+	
+	protected static void test(RemoteWebDriver driver, String url, By by, String subImageName, String imageName)
+	{
+		driver.get(url);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		byte[] bytes = driver.getScreenshotAs(OutputType.BYTES);
+    	try {
+			Files.write(bytes, new File(imageName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    	Mat image = Imgcodecs.imread(imageName);
+    	
+    	WebElement element = driver.findElement(by);
+    	Rectangle rect = element.getRect();
+    	
+    	Rect r = new Rect();
+    	/*r.height = rect.height + 40;
+    	r.width = rect.width + 40;
+    	r.x = rect.x - 20;
+    	r.y = rect.y - 20;*/
+    	int border = 14;
+    	r.height = rect.height + 2*border;
+    	r.width = rect.width + 2*border;
+    	r.x = rect.x - border;
+    	r.y = rect.y - border;
+    	Mat subImage = image.submat(r);
+    	Imgcodecs.imwrite(subImageName, subImage);
+    	
+    	
+    	HashMap<String, DescriptorMatcher> matchers = new HashMap<String, DescriptorMatcher>();
+		matchers.put("bf", org.opencv.features2d.BFMatcher.create());
+		//matchers.put("flann", org.opencv.features2d.FlannBasedMatcher.create());
+		
+		HashMap<String, Feature2D> computers = new HashMap<String, Feature2D>();
+		//computers.put("agast", org.opencv.features2d.AgastFeatureDetector.create());
+		//computers.put("akaze", org.opencv.features2d.AKAZE.create());
+		computers.put("brisk", org.opencv.features2d.BRISK.create());
+		//computers.put("gftt", org.opencv.features2d.GFTTDetector.create());
+		//computers.put("kaze", org.opencv.features2d.KAZE.create());
+		//computers.put("mser", org.opencv.features2d.MSER.create());
+		//computers.put("orb", org.opencv.features2d.ORB.create());
+		//computers.put("fast", org.opencv.features2d.FastFeatureDetector.create());
+		
+		HashMap<String, Feature2D> detectors = new HashMap<String, Feature2D>();
+		detectors.put("agast", org.opencv.features2d.AgastFeatureDetector.create());
+		//detectors.put("akaze", org.opencv.features2d.AKAZE.create());
+		//detectors.put("brisk", org.opencv.features2d.BRISK.create());
+		//detectors.put("gftt", org.opencv.features2d.GFTTDetector.create());
+		//detectors.put("kaze", org.opencv.features2d.KAZE.create());
+		//detectors.put("mser", org.opencv.features2d.MSER.create());
+		//detectors.put("orb", org.opencv.features2d.ORB.create());
+		detectors.put("fast", org.opencv.features2d.FastFeatureDetector.create());
+		
+		test(driver, subImageName, subImage, imageName, image, matchers, computers, detectors);
+		
+	}
+	
+	protected static void convert(Mat subImage, MatOfDMatch matches, MatOfKeyPoint subImagePoints, MatOfKeyPoint imagePoints)
+	{
+		Mat homo = findHomography(matches, subImagePoints, imagePoints);
+		
+		//-- Get the corners from the sub image ( the object to be "detected" )
+		MatOfPoint subImageCorners = new MatOfPoint();
+		subImageCorners.fromArray(new Point(0, 0), 
+				new Point(subImage.cols(), 0),
+				new Point(subImage.cols(), subImage.rows()),
+				new Point(0, subImage.rows()));
+		MatOfPoint imageCorners = new MatOfPoint();
+		imageCorners.fromArray(new Point(0, 0), new Point(0, 0), new Point(0, 0), new Point(0, 0));
+		
+		Core.perspectiveTransform(subImageCorners, imageCorners, homo);
+		System.out.println("converted : " + imageCorners.toArray());
+	}
+	
+	protected static Mat findHomography(MatOfDMatch matches, MatOfKeyPoint subImagePoints, MatOfKeyPoint imagePoints)
+	{
+		DMatch[] matchesArray = matches.toArray();
+		Point[] subImage = new Point[matchesArray.length];
+		Point[] image = new Point[matchesArray.length];
+		KeyPoint[] subImagePointArray = subImagePoints.toArray();
+		KeyPoint[] imagePointArray = imagePoints.toArray();
+		
+		
+		//-- Localize the sub image
+		for(int i = 0; i< matchesArray.length; i++)
+		{
+			subImage[i] = subImagePointArray[matchesArray[i].queryIdx].pt;
+			image[i] = imagePointArray[matchesArray[i].trainIdx].pt;
+		}
+		
+		MatOfPoint2f subImageMat = new MatOfPoint2f();
+		subImageMat.fromArray(subImage);
+		MatOfPoint2f imageMat = new MatOfPoint2f();
+		imageMat.fromArray(image);
+		Mat homo = Calib3d.findHomography(subImageMat, imageMat);
+		return homo;
+	}
+	
+	protected static Rect convert2(MatOfDMatch matches, MatOfKeyPoint imagePoints)
+	{
+		DMatch[] matchesArray = matches.toArray();
+		KeyPoint[] imagePointArray = imagePoints.toArray();
+		double minX = imagePointArray[matchesArray[0].trainIdx].pt.x;
+		double minY = imagePointArray[matchesArray[0].trainIdx].pt.y;
+		double maxX = imagePointArray[matchesArray[0].trainIdx].pt.x;
+		double maxY = imagePointArray[matchesArray[0].trainIdx].pt.y;
+		for(int i = 0; i< matchesArray.length; i++)
+		{
+			Point pt = imagePointArray[matchesArray[i].trainIdx].pt;
+			if(pt.x < minX)
+				minX = pt.x;
+			if(pt.x > maxX)
+				maxX = pt.x;
+			if(pt.y < minY)
+				minY = pt.y;
+			if(pt.y > maxY)
+				maxY = pt.y;
+		}
+		Rect rect = new Rect();
+		rect.x = (int) minX;
+		rect.y = (int) minY;
+		rect.width = (int)(maxX - minX);
+		rect.height = (int)(maxY - minY);
+		return rect;
+	}
 }
--- xscript.test/test.xs
+++ xscript.test/test.xs
@@ -5,7 +5,7 @@ new -class EdgeDriver -return #driver
 get -this #driver -url http://www.google.com
 findElementById -this #driver -using lst-ib -return #input
 assert -not -null #input
-//sendKeys -this #input -keysToSend abc
+// sendKeys -this #input -keysToSend abc
 findElementByName -this #driver -using btnK -return #button
 assert -not -null #button
 click -this #button
@@ -14,4 +14,8 @@ try
 catch -return #ex
     printStackTrace -this #ex
 final
+import -class xproject.xwindow.XWindow
+findWindow -class XWindow -className  -windowName Google\U+0020-\U+0020Microsoft\U+0020Edge
+import -class net.sourceforge.tess4j.Tesseract
+new -class Tesseract -return #tess
 close -this #driver
