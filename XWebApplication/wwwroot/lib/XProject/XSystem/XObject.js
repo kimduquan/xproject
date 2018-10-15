be1ca19fee878a65def3e8f0ce88ea929f9a8a61
John Doe <john@sample.com>
2018-10-16 01:43:26 +0700

    #add some js files

---
+++ XWebApplication/wwwroot/lib/XProject/XSystem/XObject.js
@@ -0,0 +1,18 @@
+﻿function XObject() {
+
+}
+
+XObject.prototype.xgetType = function () {
+}
+
+XObject.prototype.xgetHashCode = function () {
+
+}
+
+XObject.prototype.xtoString = function () {
+
+}
+
+XObject.prototype.xequals = function (other) {
+
+}
---
+++ XWebApplication/wwwroot/lib/XProject/XSystem/XType.js
@@ -0,0 +1,59 @@
+﻿function XType() {
+
+}
+
+XType.prototype.xgetConstructor = function (types) {
+
+}
+
+XType.prototype.xgetConstructors = function () {
+
+}
+
+XType.prototype.xgetInterface = function (name) {
+
+}
+
+XType.prototype.xgetInterfaces = function () {
+
+}
+
+XType.prototype.xgetMember = function (name) {
+
+}
+
+XType.prototype.xgetMembers = function () {
+
+}
+
+XType.prototype.xgetMethod = function (name) {
+
+}
+
+XType.prototype.xgetMethods = function () {
+
+}
+
+XType.prototype.xgetField = function (name) {
+
+}
+
+XType.prototype.xgetFields = function () {
+
+}
+
+XType.prototype.xgetNestedType = function (name) {
+
+}
+
+XType.prototype.xgetNestedTypes = function () {
+
+}
+
+XType.prototype.xgetProperties = function () {
+
+}
+
+XType.prototype.xgetProperty = function (name) {
+
+}
