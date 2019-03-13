d5f0e1f4b35b0fd4061cdceea2bf081de1b2304a
Kim Du Quan <kimduquan@gmail.com>
2019-03-14 01:40:18 +0700

    #add some code

--- XWebApplication/Util.cs
+++ XWebApplication/Util.cs
@@ -24,6 +24,7 @@ namespace XWebApplication
                 if (x == null)
                 {
                     x = new XInternal();
+                    x.XAssembly = XAssemblyInternal.XNew;
                 }
                 return x;
             }
@@ -57,11 +58,6 @@ namespace XWebApplication
                 route = "/" + dll + "/" + name;
             }
             return route;
-        }
-
-        public static XAssembly XGetAssembly(XType xtype)
-        {
-            return new XAssemblyInternal(xtype.X.Assembly, X);
         }
 
         public static void XToHiearchy(IEnumerator<XType> it, out Dictionary<string, List<string>> childNSs, out Dictionary<string, List<XType>> childTypes)
@@ -123,6 +119,11 @@ namespace XWebApplication
             return XToDisplayString(type.XName);
         }
 
+        public static string XToDisplayString(XAssembly a)
+        {
+            return XToDisplayString(a.XFullName.Split(',')[0]);
+        }
+
         public static string XToDisplayString(XPropertyInfo prop)
         {
             return XToDisplayString(prop.XName);
@@ -140,29 +141,63 @@ namespace XWebApplication
 
         public static string XToDisplayString(string ns)
         {
-            string result = "";
-            IEnumerator<char> ci = ns.GetEnumerator();
-            IEnumerator<char> cj = ns.GetEnumerator();
-            if(cj.MoveNext())
-            {
-                while (ci.MoveNext() && cj.MoveNext())
-                {
-                    result += ci.Current;
-                    if ((Char.IsControl(ci.Current) && !Char.IsControl(cj.Current))
-                        || (Char.IsDigit(ci.Current) && !Char.IsDigit(cj.Current))
-                        || (Char.IsLetter(ci.Current) && !Char.IsLetter(cj.Current))
-                        || (Char.IsLetterOrDigit(ci.Current) && !Char.IsLetterOrDigit(cj.Current))
-                        || (Char.IsLower(ci.Current) && !Char.IsLower(cj.Current))
-                        || (Char.IsNumber(ci.Current) && !Char.IsNumber(cj.Current))
-                        )
-                    {
-                        result += " ";
-                    }
-                }
-                result += ci.Current;
-            }
+            string result = ns;
             result = result.Replace('.', ' ');
             result = result.Replace('_', ' ');
+            result = result.Trim();
+            IEnumerator<char> current = result.GetEnumerator();
+            IEnumerator<char> next = result.GetEnumerator();
+            IEnumerator<char> prev = result.GetEnumerator();
+            bool lower = false;
+            bool whiteSpace = false;
+            bool ignore = false;
+            string temp = "";
+            if(next.MoveNext())
+            {
+                if (next.MoveNext() && current.MoveNext())
+                {
+                    while (next.MoveNext() && current.MoveNext() && prev.MoveNext())
+                    {
+                        if(!ignore)
+                        {
+                            if(lower)
+                                temp += Char.ToLower(prev.Current);
+                            else
+                                temp += prev.Current;
+                        }
+                        if (whiteSpace)
+                        {
+                            temp += ' ';
+                        }
+
+                        if ((Char.IsControl(current.Current) && !Char.IsControl(next.Current))
+                            || (Char.IsDigit(current.Current) && !Char.IsDigit(next.Current))
+                            || (Char.IsLetter(current.Current) && !Char.IsLetter(next.Current))
+                            || (Char.IsLetterOrDigit(current.Current) && !Char.IsLetterOrDigit(next.Current))
+                            || (Char.IsLower(current.Current) && !Char.IsLower(next.Current))
+                            || (Char.IsNumber(current.Current) && !Char.IsNumber(next.Current))
+                            )
+                        {
+                            lower = ignore = whiteSpace = false;
+                            whiteSpace = true;
+                        }
+                        else if((Char.IsWhiteSpace(prev.Current) || whiteSpace) && Char.IsUpper(current.Current))
+                        {
+                            lower = ignore = whiteSpace = false;
+                            lower = true;
+                        }
+                        else
+                        {
+                            lower = ignore = whiteSpace = false;
+                        }
+                    }
+                    if (prev.MoveNext())
+                        temp += prev.Current;
+                    if(current.MoveNext())
+                        temp += current.Current;
+                }
+            }
+            result = temp;
             return result;
         }
 
