7d35b0c1693c9a995c38f36f466538d8dd0a4d06
Kim Du Quan <kimduquan@gmail.com>
2019-09-14 03:48:23 +0700

    #

---
+++ XWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using Button = System.Windows.Controls.Button;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+namespace XWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Rectangle screenRect = Screen.PrimaryScreen.Bounds;
+            double width = 0;
+            double height = 0;
+            XRatio.XCalculate(screenRect.Height, screenRect.Width, out height, out width);
+            double left = (screenRect.Width - width) / 2;
+            double top = (screenRect.Height - height) / 2;
+            XRatio.XCalculate(top, left, out top, out left);
+            this.Left = left;
+            this.Top = top;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private void Button_Loaded(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            double xratio = XRatio.XGet();
+
+            double fontSize = button.FontSize;
+            double margin = fontSize / xratio;
+            Thickness marginProp = button.Margin;
+            marginProp.Top = margin / xratio / 2;
+            marginProp.Bottom = margin / xratio / 2;
+            marginProp.Left = margin * xratio / 2;
+            marginProp.Right = margin * xratio / 2;
+            button.Margin = marginProp;
+
+            double width = button.ActualWidth;
+            double height = button.ActualHeight;
+            XRatio.XCalculate(height, width, out height, out width, false);
+            button.Width = width;
+            button.Height = height;
+
+            /*int bottom = button.Bottom;
+            Rectangle bounds = button.Bounds;
+            Rectangle client = button.ClientRectangle;
+            Size clientSize = button.ClientSize;
+            Rectangle display = button.DisplayRectangle;
+            int height = button.Height;
+            int left = button.Left;
+            Point location = button.Location;
+            Padding margin = button.Margin;
+            Size maxSize = button.MaximumSize;
+            Size minSize = button.MinimumSize;
+            Padding padding = button.Padding;
+            int right = button.Right;
+            Size size = button.Size;
+            int top = button.Top;
+            int width = button.Width;*/
+        }
+    }
+}
---
+++ XWpfApp/XRatio.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+
+namespace XWpfApp
+{
+    public class XRatio
+    {
+        public static double XGet()
+        {
+            return (1 + Math.Sqrt(5)) / 2;
+        }
+
+        public static bool XEquation(double a, double b, double c, out double x1, out double x2 )
+        {
+            bool res = false;
+            if(a + b + c == 0)
+            {
+                res = true;
+                x1 = 1;
+                x2 = c / a;
+            }
+            else if(a - b + c == 0)
+            {
+                res = true;
+                x1 = -1;
+                x2 = -1 * c / a;
+            }
+            else
+            {
+                double delta = b * b - 4 * a * c;
+                x1 = 0;
+                x2 = 0;
+                if (delta > 0)
+                {
+                    res = true;
+                    x1 = (-1 * b + Math.Sqrt(delta)) / (2 * a);
+                    x2 = (-1 * b - Math.Sqrt(delta)) / (2 * a);
+                }
+                else if (delta == 0)
+                {
+                    res = true;
+                    x1 = -1 * b / (2 * a);
+                    x2 = x1;
+                }
+            }
+            return res;
+        }
+
+        public static void XCalculate(double small, double big, out double newSmall, out double newBig, bool smaller = true)
+        {
+            double xratio = XGet();
+            double multiple = small * big;
+            double newMultiple = 0;
+            if(smaller)
+                newMultiple = multiple / xratio;
+            else
+                newMultiple = multiple * xratio;
+
+            // Approach 1 : 
+            // small + big = sum (1)
+            // small * big = multiple (2)
+            // <=> big = sum - small
+            // <=> small * ( sum - small ) = multiple
+            // <=> small*sum - small*small - multiple = 0
+            // <=> a = -1, b = sum, c = -1 * multiple
+
+            // Approach 2 :
+            // big*small = multiple (1)
+            // big/small = ratio (2)
+            // <=> big = multiple / small
+            // <=> multiple / (small*small) = ratio
+            // <=> multiple = ratio*small*small
+            // <=> ratio*small*small + 0*small - multiple = 0;
+            // => a = ratio, b = 0, c = -1*multiple
+            double a = xratio;
+            double b = 0;
+            double c = -1 * newMultiple;
+            bool res = XEquation(a, b, c, out newSmall, out newBig);
+            newBig = newSmall * xratio;
+        }
+    }
+}
