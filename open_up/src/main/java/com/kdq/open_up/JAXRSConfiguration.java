e30288797174b7809b54bf9f9a121e87623e4ed8
Kim Du Quan <kimduquan@gmail.com>
2020-02-01 20:17:28 +0700

    #

---
+++ open_up/src/main/java/com/kdq/open_up/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.kdq.open_up;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
